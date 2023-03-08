cb0d2f3dd72cc5b595b597b4d3b6fc977b85c05d
Maksym Bolotin <maksym.bolotin@appsfactory.de>
2023-03-08 17:26:39 +0100

    Changes by adambalazs

--- keycloak-auth-test-0.4.2-application/src/main/java/appsfactory/keycloak.auth/application/Application.java
+++ keycloak-auth-test-0.4.2-application/src/main/java/appsfactory/keycloak.auth/application/Application.java
@@ -2,12 +2,14 @@ package appsfactory.keycloak.auth.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("appsfactory.keycloak.auth")
-@EnableJpaRepositories
+@EnableJpaRepositories("appsfactory.keycloak.auth")
+@EntityScan("appsfactory.keycloak.auth")
 public class Application {
 
   public static void main(String[] args) {
